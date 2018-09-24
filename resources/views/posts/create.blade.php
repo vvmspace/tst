dffdeb8c1bb1ec5f6bab14cb405a52c8c1a2c7fc
vvmspace <vvmspace@gmail.com>
2018-09-24 20:02:30 +0500

    textarea experimental

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,6 +7,8 @@
                 {!! Form::open(['route' => 'post.store']) !!}
                 {!! Form::label('title', 'Title') !!}
                 {!! Form::text('title', '', ['class' => 'form-control']) !!}
+                {!! Form::label('body', 'Body') !!}
+                {!! Form::textarea('body', '', ['class' => 'form-control']) !!}
                 {!! Form::label('tags_text', 'Tags') !!}
                 {!! Form::text('tags_text', '', ['class' => 'form-control']) !!}
                 {!! Form::close() !!}
