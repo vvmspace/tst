7dac40e0d2a1ef267837eb8eb7fdfe5dbdf7f572
vvmspace <vvmspace@gmail.com>
2018-09-24 19:38:48 +0500

    content markup

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.app')
 
-<div class="container">
+@section('content')<div class="container">
     <div class="row">
         <div class="col-md-12">
-            @section('content')
+
                 @forelse($posts as $post)
                     <h2>{{ $post->title }}</h2>
                     <div>{{ $post->text }}</div>
                 @empty
                     <h2>No content</h2>
                 @endforelse
-            @endsection
+
         </div>
     </div>
 </div>
+@endsection
