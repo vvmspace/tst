1d6891b36035298e49539db36d28f8fb50c7553f
vvmspace <vvmspace@gmail.com>
2018-09-24 19:30:03 +0500

    into col-md-12

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
-@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">@section('content')
     @forelse($posts as $post)
         <h2>{{ $post->title }}</h2>
         <div>{{ $post->text }}</div>
         @empty
             <h2>No content</h2>
     @endforelse
-@endsection
+@endsection</div>
+    </div>
+</div>
