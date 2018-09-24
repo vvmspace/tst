a396d8a2bb529cdfd69c42ea36b5053266d7f1f7
vvmspace <vvmspace@gmail.com>
2018-09-24 19:17:57 +0500

    Tag::CreateOrGet()

--- app/Tag.php
+++ app/Tag.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
-    //
+
+    protected $fillable = ['name', 'slug'];
+
+    static function CreateOrGet($tag_text){
+        $tag = Tag::where('name', 'like', $tag_text)->first();
+        if(empty($tag)){
+            $slug = str_slug($tag_text, '-');
+            if(!empty(Tag::where('slug', $slug)->first())){
+                // Slug был лишним для 2х-часовой задачи, можно ещё допилить транслитерацию
+                $slug .= '-' . time();
+            }
+            $tag = Tag::create(['name' => $tag_text, 'slug' => $slug]);
+        }
+        return $tag;
+    }
 }
