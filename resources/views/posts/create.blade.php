7e4fc8c3bbcc7ea23b782d684f27de5958e44aed
vvmspace <vvmspace@gmail.com>
2018-09-24 19:47:21 +0500

    posts.create

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @auth()
+                {!! Form::open(['route' => 'route.name']) !!}
+                {!! Form::label('title', 'Title') !!}
+                {!! Form::text('title') !!}
+                {!! Form::close() !!}
+                @endauth
+                @guest()
+                    <h2>You have no access here</h2>
+                @endguest
+            </div>
+        </div>
+    </div>
+@endsection
