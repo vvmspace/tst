4bb86adce76b91eb184c8393b629fe51305f844c
vvmspace <vvmspace@gmail.com>
2018-09-24 19:33:42 +0500

    fix

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,13 +2,15 @@
 
 <div class="container">
     <div class="row">
-        <div class="col-md-12">@section('content')
-    @forelse($posts as $post)
-        <h2>{{ $post->title }}</h2>
-        <div>{{ $post->text }}</div>
-        @empty
-            <h2>No content</h2>
-    @endforelse
-@endsection</div>
+        <div class="col-md-12">
+            @section('content')
+                @forelse($posts as $post)
+                    <h2>{{ $post->title }}</h2>
+                    <div>{{ $post->text }}</div>
+                @empty
+                    <h2>No content</h2>
+                @endforelse
+            @endsection
+        </div>
     </div>
 </div>
