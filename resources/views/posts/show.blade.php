2f5df141af7bce0d68174ef04de6310da2c748c2
vvmspace <vvmspace@gmail.com>
2018-09-24 20:35:36 +0500

    @auth

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,10 +3,19 @@
 @section('content')<div class="container">
     <div class="row">
         <div class="col-md-12">
+            <h1>
+                {{ $post->title }}
+            </h1>
+            <div class="body">
+                {{ $post->body }}
+            </div>
             @foreach($post->tags as $tag)
-                {{ $tag->name }}        {!! Form::open( ['method' => 'delete', 'url' => route('tag.destroy', ['tag' => $tag->id]), 'style' => 'display: inline', 'onSubmit' => 'return confirm("Вы уверены, что хотите удалить?")']) !!}
-                <button type="submit" class="btn btn-danger rb">[x]</button>
+                {{ $tag->name }}
+                @auth()
+                {!! Form::open( ['method' => 'delete', 'url' => route('tag.destroy', ['tag' => $tag->id]), 'style' => 'display: inline', 'onSubmit' => 'return confirm("Вы уверены, что хотите удалить?")']) !!}
+                <button type="submit" class="btn btn-danger">[x]</button>
                 {!! Form::close() !!}
+                @endauth
             @endforeach
         </div>
     </div>
