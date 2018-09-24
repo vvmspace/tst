722c2e767876c2ab6d2c89541a33a635c5d2a065
vvmspace <vvmspace@gmail.com>
2018-09-24 19:25:50 +0500

    / -> PostController@index

--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,14 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::get('/', 'PostController@index');
 Route::resource('post', 'PostController');
 Route::resource('tag', 'TagController');
